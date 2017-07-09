7fb348d5b57de8441e1fa20c41c4d5e825e712ef
Pressy Freeth <chrissiefdo002@gmail.com>
2017-07-09 15:39:26 +0530

    port changed

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 // server setup
 var express = require('express')
 var app = express();
-app.listen(5858,function(err){
+app.listen(3000,function(err){
   console.log("..............................................");
   if(err) console.log("error while connecting to server \n" + Date());
   else console.log("Server is running on port 3000 \n" + Date());
